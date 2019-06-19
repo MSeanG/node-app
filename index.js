c431f1a2f3d94c109eabb4bc9e725969f481a403
Sean Gilmore <gilmore@michael-sean.com>
2019-06-19 12:36:50 -0400

    Added: Get URL and parse it, get URL path, send response, log return path to console

--- index.js
+++ index.js
@@ -5,10 +5,23 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // Server will respond to all requests with a string
 const server = http.createServer((req, res) => {
+
+  // Get URL and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get URL path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
   res.end('Hello there\n');
+
+  // Log the return path
+  console.log('Request received on path: ' + trimmedPath);
 });
 
 // Start server and have it listen on port 3000
