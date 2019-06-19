9ca7f5bdde892a916a25f724b8b8764e3ea9e2ec
Sean Gilmore <gilmore@michael-sean.com>
2019-06-19 10:58:47 -0400

    Server listens on port 3000 and returns Hello there

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello there!');
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+const http = require('http');
+
+// Server will respond to all requests with a string
+const server = http.createServer((req, res) => {
+  res.end('Hello there\n');
+});
+
+// Start server and have it listen on port 3000
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
